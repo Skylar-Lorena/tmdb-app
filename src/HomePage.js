0d2840c27ff1ad3a34c56efe3f4128ec29dc78d0
Skylar-Lorena <lorayne911@gmail.com>
2024-06-17 11:32:26 +0300

    complete: fetching data and routing works

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link for navigation
 
 const HomePage = () => {
-  const logoSrc = '/public/logo.png'
+  const logoSrc = "/public/logo.png";
   return (
     <div className="home-page">
       <div className="intro">
-      <img src={logoSrc} alt="Kenya Flixx Logo" className="logo" height={300}/>
+      <img src={logoSrc} alt="Kenya Flixx Logo" className="logo" width={300}/>
         <h1>Welcome to Kenya Flixx!</h1>
         <p>Explore a vast collection of popular movies.</p>
         <Link to="/movies" className="btn btn-primary explore-btn">Explore Movies</Link>  {/* Use Link component */}
